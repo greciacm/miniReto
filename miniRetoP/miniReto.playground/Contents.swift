2c658221a473595ceb96421b5efacf1ef68d03e1
Grecia Carrion <grc_m8@hotmail.com>
2016-03-04 00:58:22 -0600

    2

---
+++ miniRetoP/miniReto.playground/Contents.swift
@@ -0,0 +1,65 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numeros = 1...100
+
+for numero in numeros {
+    
+    if (numero % 5 == 0) && (numero % 2 == 0) && (numero >= 30 && numero <= 40){
+        
+        print("#\(numero ) Bingo, Par, Viva swift!!!")
+    } else {
+        
+        if (numero % 5 == 0) && (numero % 2 != 0) && (numero >= 30 && numero <= 40){
+            
+            print("#\(numero ) Bingo, Impar, Viva swift!!")
+        }
+            
+        else
+            
+            if (numero % 5 != 0) && (numero % 2 != 0) && (numero >= 30 && numero <= 40){
+                
+                print("#\(numero ) Impar, Viva swift!!")
+                
+            }
+                
+            else
+                
+                if (numero % 5 != 0) && (numero % 2 == 0) && (numero >= 30 && numero <= 40){
+                    
+                    print("#\(numero ) Par, Viva swift!!!")
+                    
+                }
+                    
+                    
+                else
+                    
+                    if (numero % 5 == 0) && (numero % 2 != 0) && (numero < 30  || numero > 40){
+                        
+                        print("#\(numero ) Bingo, Impar")
+                        
+                    }
+                    else {
+                        
+                        if (numero % 5 == 0) && (numero % 2 == 0) && (numero >= 30 ||  numero <= 40){
+                            
+                            print("#\(numero ) Bingo, Par")
+                        }
+                            
+                        else {
+                            
+                            if (numero % 5 != 0) && (numero % 2 == 0) && (numero < 30 ||  numero > 40){
+                                
+                                print("#\(numero ) Par")
+                            }
+                                
+                            else {
+                                
+                                if (numero % 5 != 0) && (numero % 2 != 0) && (numero < 30 ||  numero > 40){
+                                    
+                                    print("#\(numero ) Impar")
+                                }
+                                
+                            }}}}}
+
