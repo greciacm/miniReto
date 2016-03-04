10d548c70baf1440489c9492445518eaf2fc1310
Grecia Carrion <grc_m8@hotmail.com>
2016-03-04 01:38:37 -0600

    aquí está el playground

--- miniRetoP/miniReto.playground/Contents.swift
+++ miniRetoP/miniReto.playground/Contents.swift
@@ -4,12 +4,17 @@
 
 import UIKit
 
+
+// numeros del 0 al 100
 var numeros = 0...100
 
+// uso del for
 for numero in numeros {
+    
     
     if (numero % 5 == 0) && (numero % 2 == 0) && (numero >= 30 && numero <= 40){
         
+        //imprime en consola
         print("#\(numero ) Bingo, Par, Viva swift!!!")
     } else {
         
